7a905846004533e45cc256009c49b006b7eccf7b
unknown <enzoassis256@gmail.com>
2024-06-26 10:43:22 -0300

    adicionando exerci

---
+++ js/26-06/prime.js
@@ -0,0 +1,4 @@
+console.log("teste");
+console.log("Enzo");
+console.log("ESta é a nossa primeira impressão na tela");
+
---
+++ js/26-06/variavel.js
@@ -0,0 +1,9 @@
+let mensagem = "olá Enzo";
+let nome; // ele cria uma variavel indefinida, sem valor 
+let disciplina = null;// cria uma variavel vazia. Null
+let texto = "Este é um exemplo"
+
+
+console.log(nome);
+console.log(disciplina);
+console.log(texto); 
