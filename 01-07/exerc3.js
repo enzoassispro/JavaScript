5aeda7673b34a0f47f8a28d8de057d0f9f954a98
enzoassispro <enzoassis256@gmail.com>
2024-07-01 11:41:58 -0300

    adicionando exerc3

---
+++ 01-07/exerc3.js
@@ -0,0 +1,7 @@
+const cities = ['São paulo', 'Rio de Janeiro', 'Minas Gerais']
+let upperCaseCities = [];
+
+for( let i = 0; i <cities.length; i++){
+    upperCaseCities.push(cities[i].toUpperCase());
+}
+console.log(upperCaseCities);
